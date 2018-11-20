3ae798ce4f666c06bd892907022a9419f8cee7eb
stiliyan <stili.hristov@gmail.com>
2018-11-20 14:48:26 +0200

    Add docs for methods in ExredisKeyAuth

--- lib/exredis_key_auth.ex
+++ lib/exredis_key_auth.ex
@@ -5,6 +5,9 @@ defmodule ExredisKeyAuth do
   """
   import Exredis
 
+  @doc """
+  Gets a corresponding work_group_id in relation to the keys
+  """
   def get(keys) do
     {:ok, client} = start_link()
 
@@ -19,6 +22,9 @@ defmodule ExredisKeyAuth do
     end
   end
 
+  @doc """
+  Sets a key/work_group pair
+  """
   def set(keys, work_group_id) do
    {:ok, client} = start_link()
 
