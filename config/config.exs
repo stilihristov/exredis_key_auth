b7067a578b4d42bf33406417f75ea0352f34b9fc
stiliyan <stili.hristov@gmail.com>
2018-11-28 17:38:08 +0200

    No longer requires pass for REDIS connection

--- config/config.exs
+++ config/config.exs
@@ -10,7 +10,6 @@ use Mix.Config
 config :exredis,
   host: System.get_env("REDIS_HOST"),
   port: System.get_env("REDIS_PORT"),
-  password: System.get_env("REDIS_PASS"),
   db: 0,
   reconnect: :no_reconnect,
   max_queue: :infinity
